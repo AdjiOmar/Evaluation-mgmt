882ee1c371795088eabd23d95775efc14397007e
Baye LAM <bayesabarane@gmail.com>
2022-10-23 21:42:30 +0000

    update backend for time serialization, evaluation and question issue

--- src/main/java/com/niit/evaluation_mgmt/controller/QuestionController.java
+++ src/main/java/com/niit/evaluation_mgmt/controller/QuestionController.java
@@ -1,9 +1,16 @@
 package com.niit.evaluation_mgmt.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+
+import com.niit.evaluation_mgmt.model.Evaluation;
 import com.niit.evaluation_mgmt.model.Question;
+import com.niit.evaluation_mgmt.service.EvaluationService;
 import com.niit.evaluation_mgmt.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,34 +28,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 public class QuestionController {
 
+    @Autowired
+    private EvaluationService evaluationService;
     private final QuestionService service;
 
     public QuestionController(QuestionService service) {
         this.service = service;
     }
-@PostMapping
-public Question create(@RequestBody Question question) {
-   return service.create(question);
-}
+    @PostMapping
+    public ResponseEntity<Question> create(@RequestBody Question question)
+    {
+       return ResponseEntity.ok(service.create(question));
+    }
 
-@GetMapping
-public List<Question> getAll() {
-    return service.getAll();
-}
+    @GetMapping
+    public List<Question> getAll() {
+        return service.getAll();
+    }
 
-@GetMapping(value = "/{id}")
-public Optional<Question> getById(@PathVariable Long id){
-    return service.getById(id);
-}
+    @GetMapping("eval/{evalId}")
+    public List<Question> getQuestionsOfEvaluation(@PathVariable("evalId") Long evalId) {
+        Optional<Evaluation> evaluation = this.evaluationService.getById(evalId);
+        Set<Question> questions = evaluation.get().getQuestions();
+        List list = new ArrayList(questions);
+        return list;
+    }
 
-@PutMapping
-public Question update(@RequestBody Question question) {
-    return service.update(question);
-}
+    @GetMapping(value = "/{id}")
+    public Optional<Question> getById(@PathVariable Long id){
+        return service.getById(id);
+    }
 
-@DeleteMapping(value="/{id}")
-public void delete(@PathVariable Long id){
-    service.delete(id);
-}
+    @PutMapping
+    public Question update(@RequestBody Question question) {
+        return service.update(question);
+    }
+
+    @DeleteMapping(value="/{id}")
+    public void delete(@PathVariable Long id){
+        service.delete(id);
+    }
 
 } 
--- src/main/java/com/niit/evaluation_mgmt/repository/QuestionRepository.java
+++ src/main/java/com/niit/evaluation_mgmt/repository/QuestionRepository.java
@@ -1,11 +1,15 @@
 package com.niit.evaluation_mgmt.repository;
 
+import com.niit.evaluation_mgmt.model.Evaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.niit.evaluation_mgmt.model.Question;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long>{
-    
+    List<Question> findByEvaluation(Evaluation evaluation);
+
 }
